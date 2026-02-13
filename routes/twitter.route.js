5027e4ead58d8c6fefac73962595057a7d790e93
PaNkaj44 <bamniya853@gmail.com>
2026-02-13 15:04:54 +0530

    add twitter router

--- routes/twitter.route.js
+++ routes/twitter.route.js
@@ -2,24 +2,35 @@ const express = require("express")
 
 const router = express.Router()
 
-router.get("/", async (req, res) => {
-    const codeVerifier = generateCodeVerifier();
-    const codeChallenge = generateCodeChallenge(codeVerifier);
+const crypto = require("crypto")
+
+router.get("/", (req, res) => {
+    const clientId = process.env.TWITTER_CLIENT_ID;
+    const redirectUri = process.env.TWITTER_REDIRECT_URI;
+
+    const codeVerifier = crypto.randomBytes(32).toString("hex");
+
+    const codeChallenge = crypto
+        .createHash("sha256")
+        .update(codeVerifier)
+        .digest("base64url");
+
 
     req.session.codeVerifier = codeVerifier;
 
     const authUrl =
         "https://twitter.com/i/oauth2/authorize" +
         "?response_type=code" +
-        `&client_id=${process.env.TWITTER_CLIENT_ID}` +
-        `&redirect_uri=${encodeURIComponent(process.env.TWITTER_REDIRECT_URI)}` +
-        "&scope=tweet.read%20tweet.write%20users.read%20offline.access" +
-        "&state=twitter_state" +
-        `&code_challenge=${codeChallenge}` +
+        "&client_id=" + clientId +
+        "&redirect_uri=" + encodeURIComponent(redirectUri) +
+        "&scope=" + encodeURIComponent("tweet.read users.read offline.access") +
+        "&state=state123" +
+        "&code_challenge=" + codeChallenge +
         "&code_challenge_method=S256";
 
-    res.json({ url: authUrl });
+    res.redirect(authUrl);
 });
+
 
 
 router.get("/callback",);
